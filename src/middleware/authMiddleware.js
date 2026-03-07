edf72ea89045805b1c2607c9b8ea114abf5ceecb
dcablorh <dcablorh@gmail.com>
2026-03-07 22:38:41 +0000

    assignment subission

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -11,6 +11,22 @@ import User from "../models/User.js";
 
 const authMiddleware = async (req, res, next) => {
   //  implement here
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+  const token = authHeader.substring(7);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 };
 
 export default authMiddleware;
--- src/models/User.js
+++ src/models/User.js
@@ -12,6 +12,15 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   // Students implement
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    }
 });
 
 const User = mongoose.model("User", userSchema);
